82921d47ef3c9ac7324ef8f160496464133be159
amaansrc <amaan_shahid@ymail.com>
2026-07-02 14:55:51 +0530

    Initial commit

---
+++ src/test/java/com/example/FlightMgmtSys/FlightMgmtSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FlightMgmtSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightMgmtSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
